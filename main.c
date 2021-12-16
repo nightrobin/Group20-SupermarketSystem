454e7014f8fbaa386f318076c0857de6524953cd
nightrobin <1894201+nightrobin@users.noreply.github.com>
2021-12-16 13:31:55 +0800

    Initial Commit
    Hi members, This will be our base code.

---
+++ main.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "others.h"
+
+// using namespace stdio;
+
+int main()
+{
+    int number = 2;
+
+    datatype<space>identifier[size] = initialization;
+                       0  1  2
+    int numbers[3] = { 5, 6, 8 };
+
+
+    printf("%d", number);
+    printf("%d", numbers[0]); // 5
+
+}
+
